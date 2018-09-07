2646a6d2ad091612ebb6ec5996721d72898ce8cc
onionpower <chocoonion@gmail.com>
2018-09-07 21:27:24 +0300

    dummy consumer and producer

--- src/kafka-stopwatch/Program.cs
+++ src/kafka-stopwatch/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
 
 namespace kafka_stopwatch
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var kafkaConfig = new Dictionary<string, object>
+            {
+                {"bootstrap.servers", "localhost:9092"},    
+                { "group.id", Guid.NewGuid() },
+                { "auto.offset.reset", "smallest" }
+            };
+
+            var cts = new CancellationTokenSource();
+            var ct = cts.Token;
+            var pTask = Task.Run(() =>
+            {
+                using (var kp = new Producer<Null, string>(
+                    kafkaConfig,
+                    new NullSerializer(),
+                    new StringSerializer(Encoding.UTF8)))
+                {
+                    //do nothing for a moment
+                }
+            }, ct);
+
+            var cTask = Task.Run(() =>
+            {
+                using (var kc = new Consumer<Null, string>(
+                    kafkaConfig,
+                    new NullDeserializer(),
+                    new StringDeserializer(Encoding.UTF8)))
+                {
+                    kc.Subscribe("hello-topic");
+                    while (true)
+                    {
+                        if (!kc.Consume(out var msg, 100))
+                        {
+                            break;
+                        }                        
+                        Console.WriteLine(msg.Value);
+                    }
+                }
+            }, ct);
+            Task.WaitAll(cTask, pTask);
+            Console.WriteLine("all the castles are conqured and the dragons are banished");
         }
     }
 }
