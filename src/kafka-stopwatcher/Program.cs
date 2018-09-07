6e838bec44d0484c934bf96e77c7981722f82c2f
onionpower <chocoonion@gmail.com>
2018-09-07 18:18:06 +0300

    init

---
+++ src/kafka-stopwatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kafka_stopwatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
